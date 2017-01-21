862d034c6c1424d44e03ef7c7d0ebb71e78b1c3a
mguida22 <michael.guida@colorado.edu>
2017-01-21 13:59:21 -0500

    semi-working camera

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
 
 class Home extends Component {
-  render () {
+  constructor() {
+    super();
+    this.state = {
+      qrData: null
+    };
+  }
+
+  onBarCodeRead(e) {
+    console.log(e.data);
+
+    this.setState({
+      qrData: e.data
+    });
+  }
+
+  render() {
     return (
-      <View>
-        <Text>Home</Text>
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+          onBarCodeRead={this.onBarCodeRead.bind(this)}
+          barCodeTypes={['qr']}>
+        </Camera>
+        <Text>{this.state.qrData}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+
 
 export default Home;
