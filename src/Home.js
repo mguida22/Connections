10489eb15e7633e744f7322ad6b1d569cd59eb5d
Samay Shamdasani <techboss302@gmail.com>
2017-01-21 10:47:23 -0500

    qr stuff

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,74 @@
+
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
-
-import Camera from 'react-native-camera';
-
-class Home extends Component {
-  render () {
+ 
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  NavigatorIOS,
+  TouchableOpacity,
+  TouchableHighlight,
+  Linking,
+} from 'react-native';
+ 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+ 
+class QR extends Component {
+  onSuccess(e) {
+    Linking.openURL(e.data).catch(err => console.error('An error occured', err))
+  }
+ 
+  render() {
     return (
-
-      <View>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          aspect={Camera.constants.Aspect.fit}>
-        </Camera>
-      </View>
-    );
+      <NavigatorIOS
+        initialRoute={{
+          component: QRCodeScanner,
+          title: 'Scan Connections Code',
+          passProps: {
+            onRead: this.onSuccess.bind(this),
+            topContent: <Text style={styles.centerText}>Scan to add social media</Text>,
+            bottomContent: <TouchableOpacity style={styles.buttonTouchable}><Text style={styles.buttonText}>OK. Got it!</Text></TouchableOpacity>
+          }
+        }}
+        style={{flex: 1}}
+      />
+    )
   }
 }
-
-
-export default Home;
+ 
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink',
+    borderRadius: 3,
+    padding: 32,
+    width: 100,
+    marginTop: 64,
+    marginBottom: 64,
+  },
+ 
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+ 
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+ 
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+ 
+  buttonTouchable: {
+    padding: 16,
+  },
+});
+ 
+export default QR;
---
+++ src/QR.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+
+class QR extends Component {
+  render() {
+    return (
+    <Text>QR Code </Text>
+    )
+  };
+}
+
+export default QR;
