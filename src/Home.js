4b14ad1ab35ba462622abd3f88266763c7807d8f
Samay Shamdasani <techboss302@gmail.com>
2017-01-21 02:26:29 -0500

    home.js camera

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-elements';
 
+import Camera from 'react-native-camera';
+
 class Home extends Component {
   render () {
     return (
+
       <View>
-        <Text>Home</Text>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          aspect={Camera.constants.Aspect.fit}>
+        </Camera>
       </View>
     );
   }
 }
+
 
 export default Home;
