46b62766451d0b075748012a97be60e93763221b
Samay Shamdasani <techboss302@gmail.com>
2017-01-21 23:01:02 -0500

    ig + linkedin auth

--- src/Profile.js
+++ src/Profile.js
@@ -4,52 +4,64 @@ import {
   StyleSheet,
   TextInput, 
   View,
+  Linking,
   Alert
 } from 'react-native';
 
 import { Text, SocialIcon } from 'react-native-elements';
 
-import simpleAuthClient from 'react-native-simple-auth';
 import secrets from './secrets';
 
 
 class ConnectionsRegister extends Component {
-  onButtonPress() {
-    Alert.alert('Button Pressed!');
+  onButtonPress(service) {
+    if (service === "instagram") {
+    Linking.openURL("https://api.instagram.com/oauth/authorize/?response_type=code&redirect_uri=https://connections.shamdasani.org&client_id="+secrets.instagram.client_id)
+
+  } else if (service === "facebook") {
+
+
+  } else if (service === "twitter") {
+
+  } else { //linkedin 
+      Linking.openURL("https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id="+secrets["linkedin-web"].client_id+"&redirect_uri=https://connections.shamdasani.org&state=DCEeFWf45A53sdfKef424")
   }
+
+};
+
 
   render() {
     return (
      <View style={styles.register}>        
        <SocialIcon style={styles.social}
-    	  title='Sign in with Twitter'
-    	  button
-    	  type='twitter'
-        onPress={this.onButtonPress}
-    	/>
+        title='Sign in with Twitter'
+        button
+        type='twitter'
+        onPress={this.onButtonPress.bind(this, 'twitter')}
+      />
 
-    	<SocialIcon style={styles.social}
-    	  title='Sign In With Facebook'
-    	  button
-    	  type='facebook'
-        onPress={this.onButtonPress}
-    	/>
+      <SocialIcon style={styles.social}
+        title='Sign In With Facebook'
+        button
+        type='facebook'
+        onPress={this.onButtonPress.bind(this, 'facebook')}
+      />
 
-    	<SocialIcon style={styles.social}
-    	  title='Sign In With Instagram'
-    	  button
-    	  light
-    	  type='instagram'
-        onPress={this.onButtonPress}
-    	/>
+      <SocialIcon style={styles.social}
+        title='Sign In With Instagram'
+        button
+        light
+        type='instagram'
+        onPress={this.onButtonPress.bind(this, 'instagram')}
+      />
 
-    	<SocialIcon style={styles.social}
-    	  title='Sign In With LinkedIn'
-    	  button
-    	  dark
-    	  type='linkedin'
+      <SocialIcon style={styles.social}
+        title='Sign In With LinkedIn'
+        button
+        dark
+        type='linkedin'
         onPress={this.onButtonPress}
-    	/>
+      />
     </View>
     );
   }
@@ -71,11 +83,11 @@ class Profile extends Component {
   render() {
     return (
      <View style={styles.view}>
-  		<Text style={styles.logo}>
-        	Connections 
+      <Text style={styles.logo}>
+          Connections 
         </Text>
         <Text style={styles.welcome}>
-        	Connect with anyone on all social media platforms within seconds
+          Connect with anyone on all social media platforms within seconds
         </Text>
          <ConnectionsRegister />
 
@@ -86,12 +98,12 @@ class Profile extends Component {
 
 const styles = StyleSheet.create({
   view: {
-  	backgroundColor: '#f8f8f8',
-  	flex: 1,
+    backgroundColor: '#f8f8f8',
+    flex: 1,
   },
   logo: {
-  	marginTop: 50,
-  	fontSize: 25,
+    marginTop: 50,
+    fontSize: 25,
     textAlign: 'center',
     fontFamily: 'Avenir Next',
 
@@ -108,8 +120,8 @@ const styles = StyleSheet.create({
 
   },
   social: {
-  	marginRight:40,
-  	marginLeft:40,
+    marginRight:40,
+    marginLeft:40,
   }
 });
 
